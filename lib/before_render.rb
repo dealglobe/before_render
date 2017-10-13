8808bc0cbfbe67981e5eff4c642138d692ca40ef
Johnson Qu <johnson.qu@dealglobe.com>
2017-10-13 17:29:40 +0800

    compatible with rails 5.1

--- lib/before_render.rb
+++ lib/before_render.rb
@@ -1,4 +1,3 @@
 require 'before_render/version'
 require 'active_support/concern'
 require 'before_render/before_render'
-require 'before_render/before_render_instance'
--- lib/before_render/before_render.rb
+++ lib/before_render/before_render.rb
@@ -1,30 +1,44 @@
 module BeforeRender
   extend ActiveSupport::Concern
 
-  module ClassMethods
-    def append_before_render_filter(*names, &blk)
-      _insert_callbacks(names, blk) do |name, options|
-        set_callback(:render, :before, name, options)
-      end
+  included do
+    define_callbacks :render, terminator: ->(controller, result_lambda) { result_lambda.call if result_lambda.is_a?(Proc); controller.performed? },
+                     skip_after_callbacks_if_terminated: true
+  end
+
+  def render(*args)
+    run_callbacks(:render) do
+      super
     end
+  end
 
-   def prepend_before_render_filter(*names, &blk)
-     _insert_callbacks(names, blk) do |name, options|
-       set_callback(:render, :before, name, options.merge(:prepend => true))
-     end
-   end
+  module ClassMethods
+    [:before, :after, :around].each do |callback|
+      define_method "#{callback}_render" do |*names, &blk|
+        _insert_callbacks(names, blk) do |name, options|
+          set_callback(:render, callback, name, options)
+        end
+      end
 
-   def skip_before_render_filter(*names, &blk)
-     _insert_callbacks(names, blk) do |name, options|
-       skip_callback(:render, :before, name, options)
-     end
-   end
+      define_method "prepend_#{callback}_render" do |*names, &blk|
+        _insert_callbacks(names, blk) do |name, options|
+          set_callback(:render, callback, name, options.merge(prepend: true))
+        end
+      end
 
-  alias_method :before_render, :append_before_render_filter
-  alias_method :prepend_before_render, :prepend_before_render_filter
-  alias_method :skip_before_render, :skip_before_render_filter
- end
+      # Skip a before, after or around callback. See _insert_callbacks
+      # for details on the allowed parameters.
+      define_method "skip_#{callback}_render" do |*names|
+        _insert_callbacks(names) do |name, options|
+          skip_callback(:render, callback, name, options)
+        end
+      end
+
+      # *_action is the same as append_*_action
+      alias_method :"append_#{callback}_render", :"#{callback}_render"
+    end
+  end
 
 end
 
-AbstractController::Base.send :include,  BeforeRender
+AbstractController::Callbacks.include BeforeRender
